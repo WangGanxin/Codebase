42308d4bea3d477b4f36fa37f1c99d150a944138
守望君 <loveqdm@qq.com>
2017-02-07 21:06:57 +0800

    update StringUtil

--- app/src/main/java/com/ganxin/codebase/utils/string/StringUtil.java
+++ app/src/main/java/com/ganxin/codebase/utils/string/StringUtil.java
@@ -60,6 +60,21 @@ public class StringUtil {
     }
 
     /**
+     * 手机号验证
+     * @param mobile
+     * @return
+     */
+    public static boolean isMobile(String mobile) {
+        Pattern p = null;
+        Matcher m = null;
+        boolean b = false;
+        p = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$"); // 验证手机号
+        m = p.matcher(mobile);
+        b = m.matches();
+        return b;
+    }
+
+    /**
      * 电话号码验证
      *
      * @param str
